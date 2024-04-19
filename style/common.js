b68a6f1a37bc5fdd9e802922e7e29833fa078256
mingggggkkkkkk <160981132+mingggggkkkkkk@users.noreply.github.com>
2024-04-19 17:12:17 +0900

    Add files via upload

--- style/common.js
+++ style/common.js
@@ -1,23 +1,4 @@
-function resizing() {
-
-}
-resizing();
-$(window).on('resize', function(){
-    //pc
-    if( $(window).innerWidth() >= 1600 ){
-        $('#gnbWrap .sub').show();
-        $('#gnbWrap').removeClass('mobile')
-        $('.close').hide();
-    }
-    //mobile
-    else {
-        $("#gnbWrap .sub").hide();
-        $(".open").show();
-        $(".close").hide();
-    }
-})
-
-//pc menu
+//pc 메뉴 활성화
 $("#gnbWrap").on('mouseenter', function(){
     $("#header").addClass('on')
 })
@@ -25,20 +6,53 @@ $("#header").on('mouseleave', function(){
     $(this).removeClass('on');
 })
 
-//mobile menu
-$(".open").on('click', function(){
-    $("#gnbWrap").addClass('mobile');
-    $(this).hide();
-    $('.close').show();
-})
-$(".close").on('click', function(){
-    $("#gnbWrap").removeClass("mobile");
-    $(this).hide();
-    $('.open').show();
-    $('#gnbWrap .snb').hide();
+//네비 메인오면 따라오기
+    $(window).scroll(function() {
+        if ($(this).scrollTop() > 500 ) {
+            $('#header').css({
+                position: 'fixed',
+                backgroundColor: '#0038C8'
+            });
+        } else {
+            $('#header').css({
+                position: 'absolute',
+                backgroundColor: 'transparent'
+            });
+        }
+    });
+
+
+
+
+//slider들 모양(테두리 생성)
+$(".slider a").on('click', function(){
+    $(this).css({border: "1px solid #000"})
 })
 
-$(document).on('click', '#gnbWrap.mobile > ul > li', function(){
-    $(this).children(".snb").slideToggle()
-            .parents().siblings().children('.snb').slideUp();
+// 헤더 자동 슬라이드와 버튼 슬라이드
+$(document).ready(function(){
+    let currentIndex = 0;
+    let slides = $('#slide ul li');
+    let totalSlides = slides.length;
+
+    function changeSlide(index) {
+        slides.removeClass('active').hide();
+        slides.eq(index).fadeIn().addClass('active');
+        currentIndex = index;
+    }
+
+    function moveToSlide(index) {
+        slides.hide().eq(index).fadeIn().addClass('active');
+        currentIndex = index;
+    }
+
+    $('#slideWrap .slider a').click(function(){
+        let index = $(this).index();
+        changeSlide(index);
+    });
+
+    setInterval(function() {
+        let nextIndex = (currentIndex + 1) % totalSlides;
+        changeSlide(nextIndex);
+    }, 5000);
 })
